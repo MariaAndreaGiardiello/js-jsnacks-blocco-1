994b6a660f6d9dd89c5a49084be94a817fa20860
MariaAndreaGiardiello <giardiello.m.a@icloud.com>
2022-05-04 12:30:20 +0200

    aggiunto correzione ma chiedere oggi

--- snack-3/main.js
+++ snack-3/main.js
@@ -1,14 +1,15 @@
 // Il software deve chiedere per 10 volte all’utente di inserire un numero. 
 // Il programma stampa la somma di tutti i numeri inseriti.
 
-// 1. Creo variabile dove inserire i numeri da sommare e definisco tipologia
-let numero = Number
-console.log(numero)
+//1. creo costante per risultato
+const totale = 0
 
 // 2. creo ciclo for per ripetere l'operazione n volte
-for (i = 0; i <= 10; i++){
-    numero = prompt("Inserisci un numero")
+for (i = 0; i <= 2; i++){
+// 3. Creo variabile dove inserire i numeri da sommare
+    const numero = Number(prompt("Inserisci un numero"))
+// 4. calcolo il risultato.
+    totale += numero
 }
-
-// 3. scrivo risultato in console.
-console.log(numero + numero + numero + numero + numero + numero + numero + numero + numero + numero)
+// 5. scrivo risultato in console.
+console.log(totale);
