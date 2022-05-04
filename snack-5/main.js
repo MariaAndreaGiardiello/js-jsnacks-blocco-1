cead96806d74c98189a57ea372d99c8b1f85b0b3
MariaAndreaGiardiello <giardiello.m.a@icloud.com>
2022-05-04 12:56:12 +0200

    ciclo funzionante attesa correzione per visua num in array

---
+++ snack-5/main.js
@@ -0,0 +1,16 @@
+//Crea un array vuoto.
+//Chiedi per 6 volte all’utente di inserire un numero, se è dispari inseriscilo nell’array.
+ 
+
+//2. Creo costante per array vuoto
+ const array = []
+
+//1. Creo ciclo for per permettere inserimento del numero da parte dell'utente n volte.
+for (i = 0; i <= 6; i++){
+    //3. creo costante per inserimento numeri
+    const numero = Number(prompt("inserisci un numero"))
+    if (numero % 2 === 1){
+        //4. stampo numeri pari in variabile "numero"
+        array[0];
+    }
+}
