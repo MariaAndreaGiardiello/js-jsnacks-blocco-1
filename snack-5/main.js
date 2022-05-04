524969572968071ab8b60069d7902456e5c6f0ea
MariaAndreaGiardiello <giardiello.m.a@icloud.com>
2022-05-04 12:59:12 +0200

    aggiunta correzione

--- snack-5/main.js
+++ snack-5/main.js
@@ -2,15 +2,17 @@
 //Chiedi per 6 volte all’utente di inserire un numero, se è dispari inseriscilo nell’array.
  
 
-//2. Creo costante per array vuoto
+//1. Creo costante per array vuoto
  const array = []
 
-//1. Creo ciclo for per permettere inserimento del numero da parte dell'utente n volte.
+//2. Creo ciclo for per permettere inserimento del numero da parte dell'utente n volte.
 for (i = 0; i <= 6; i++){
     //3. creo costante per inserimento numeri
     const numero = Number(prompt("inserisci un numero"))
     if (numero % 2 === 1){
-        //4. stampo numeri pari in variabile "numero"
-        array[0];
+        //4. stampo numeri dispari in variabile array
+        array.push(numero);
     }
 }
+// 5. visualizzo numeri dispari su console.
+console.log(array)
