46c835faf8196a967c23114be841a6aceea397a5
MariaAndreaGiardiello <giardiello.m.a@icloud.com>
2022-05-04 16:23:37 +0200

    soluzione

--- snack-6/main.js
+++ snack-6/main.js
@@ -1,1 +1,10 @@
 //Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.
+
+//1. creo una costante per inserire il numero
+const numero = Number(prompt('inserisci un numero'));
+console.log(numero)
+
+// 2. creo un ciclo if per la stampa del numero dispari + 1
+if (numero % 2 === 1){
+    console.log(numero+1)
+}
