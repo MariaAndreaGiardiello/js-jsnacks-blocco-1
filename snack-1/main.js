3bd45474c250f91ed00c1f1c0815d6533a6be910
MariaAndreaGiardiello <giardiello.m.a@icloud.com>
2022-05-04 11:41:01 +0200

    aggiunto correzioni per perfezionare ciclo

--- snack-1/main.js
+++ snack-1/main.js
@@ -8,9 +8,6 @@ console.log(firstNumber)
 const secondNumber = prompt("inserisci un numero")
 console.log(secondNumber)
 
-const box = document.querySelector('.box');
-console.log(box)
-
 // 3. creo condizione e specifico il numero da mostrare, ovvero quello maggiore
 if (firstNumber > secondNumber) {
     console.log(firstNumber)
