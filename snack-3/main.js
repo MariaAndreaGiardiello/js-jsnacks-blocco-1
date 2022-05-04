23ce8cf47d8711e184d2141e05d2e9e35f187f42
MariaAndreaGiardiello <giardiello.m.a@icloud.com>
2022-05-04 13:01:03 +0200

    aggiunto piccole correzioni

--- snack-3/main.js
+++ snack-3/main.js
@@ -2,10 +2,10 @@
 // Il programma stampa la somma di tutti i numeri inseriti.
 
 //1. creo costante per risultato
-const totale = 0
+let totale = 0
 
 // 2. creo ciclo for per ripetere l'operazione n volte
-for (i = 0; i <= 2; i++){
+for (i = 0; i <= 10; i++){
 // 3. Creo variabile dove inserire i numeri da sommare
     const numero = Number(prompt("Inserisci un numero"))
 // 4. calcolo il risultato.
